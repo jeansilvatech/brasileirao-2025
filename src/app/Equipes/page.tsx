53f94655c194a838d605ed85c27210e159f10201
jeansilvatech <jeanpsilva10@gmail.com>
2025-03-02 12:56:21 -0300

    [feat] alimentando a rota de equipes e ajuste de reponsividade

--- src/app/Equipes/page.tsx
+++ src/app/Equipes/page.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+"use client"
+import Image from "next/image";
+import React, {useEffect, useState} from "react";
 
 const Teams = ()=>{
+    const [teams, setTeams] = useState<any>(null);
+    useEffect(() => {
+        const apiTeams = async ()=>{
+            try{
+                const response = await fetch('data/teams.json');
+                const data = await response.json();
+                setTeams(data)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        apiTeams();
+    }, []);
     return(
-        <div className="bg-black/85 w-full h-full flex justify-between">
-            <div className="lg:h-full h-24 lg:w-40 w-full absolute lg:l-0 flex justify-center items-center">
+        <div className="bg-black/85 w-full h-full flex justify-between items-center lg:flex-row flex-col">
+            <div className="lg:h-full h-24 lg:w-40 w-screen flex justify-center items-center">
                 <h1 className="lg:text-7xl text-4xl text-white lg:-rotate-90 uppercase">Equipes</h1>
             </div>
-            <div className="h-full w-full"></div>
+            <div className=" h-full w-full flex justify-center items-center">
+                {
+                    teams?(
+                        <div className="w-3/4 flex justify-start items-center flex-wrap">
+                            {
+                                teams.map((item:any, index:number)=>(
+                                    <div key={index} className="m-2">
+                                        <Image className="lg:w-24 w-12" width={90} height={90} src={`/assets/${item.url}.svg`} alt=""/>
+                                    </div>
+                                ))
+                            }
+                        </div>
+
+                    ):(
+                        ''
+                    )
+            }
+            </div>
         </div>
     )
 }
