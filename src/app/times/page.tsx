1183eefbb49f89960a5a86f55ac37c51a205e10b
jeansilvatech <jeanpsilva10@gmail.com>
2025-03-26 22:55:59 -0300

    [style] ajuste de dimensões de escudo

--- src/app/times/page.tsx
+++ src/app/times/page.tsx
@@ -35,7 +35,7 @@ const Teams = ()=>{
                             {
                                 teams.map((item:any, index:number)=>(
                                     <div key={index} onClick={()=>{router.push(`por_time?name=${item.url}`)}} data-name={`${item.name}`} className="animate-enter m-2 lg:p-2 border-[1px] border-transparent relative hover:border-[1px] hover:bg-yellow-500 hover:backdrop-blur-3xl" title={item.name.toUpperCase()}>
-                                        <Image unoptimized className="lg:w-24 lg:h-24 w-12" width={90} height={90} src={`/assets/${item.url}.svg`} alt=""/>
+                                        <Image unoptimized className="lg:w-24 lg:h-24 w-12 h-12" width={90} height={90} src={`/assets/${item.url}.svg`} alt=""/>
                                     </div>
                                 ))
                             }
