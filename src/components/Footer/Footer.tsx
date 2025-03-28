6dfa1465d4db6f5a2a1198fa795a86200c46d98a
jeansilvatech <jeanpsilva10@gmail.com>
2025-03-27 23:33:15 -0300

    [style] add logo no footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import logoJS from '../../../public/logojs.webp'
+import Image from "next/image";
 
 const Footer = ()=>{
 
     const date = new Date();
     return(
         <footer className="fixed w-full p-2 bottom-0 left-0 font-light text-sm flex justify-center items-center flex-col">
-            <p className="font-light text-sm">desenvolvido por <span className="font-bold">Jean</span></p>
-            <p>{date.getFullYear()}</p>
+            <p className="font-light text-sm flex justify-center items-center">desenvolvido por <a href="https://jeansilvatech.vercel.app" target="_blank"><Image className="mx-1" unoptimized src={logoJS} width={35} height={35} alt="logo Jean Silva Tech"/></a></p>
+            <div className="w-1/6 h-[1px] bg-white/70 mb-2 rounded-[100%]"></div>
+            <p className="text-xs text-yellow-500">{date.getFullYear()}</p>
         </footer>
     )
 }
