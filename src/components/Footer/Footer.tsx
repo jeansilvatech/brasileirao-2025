42c8cc9d1a7ba7cb18c2488d2c3d29ae244479df
jeansilvatech <jeanpsilva10@gmail.com>
2025-07-26 13:54:57 -0300

    [style] remoção de linha do footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = ()=>{
     return(
         <footer className="fixed w-full p-2 bottom-0 left-0 font-light text-sm flex justify-center items-center flex-col">
             <p className="font-light text-sm flex justify-center items-center">desenvolvido por <a href="https://jeansilvatech.vercel.app" target="_blank"><Image className="mx-1" unoptimized src={logoJS} width={35} height={35} alt="logo Jean Silva Tech"/></a></p>
-            <div className="w-1/6 h-[1px] bg-white/70 mb-2 rounded-[100%]"></div>
+            
             <p className="text-xs text-yellow-500">{date.getFullYear()}</p>
         </footer>
     )
